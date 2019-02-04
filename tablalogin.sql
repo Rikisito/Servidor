ddb712cb0f26c63a13ff975ccfbc9ac25105a1e0
Rikisito <46540697+Rikisito@users.noreply.github.com>
2019-02-04 19:45:15 +0100

    Update tablalogin.sql

--- tablalogin.sql
+++ tablalogin.sql
@@ -4,3 +4,7 @@ nombre VARCHAR(20) NOT NULL,
 apellidos VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 PRIMARY KEY (id))DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+             
+             
+    /*Esto es para que funcione el login*/
+             Alter table login modify clave varchar(255);
