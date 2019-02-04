6b2d7051e7b49c5cf081a11177af313016abae71
Rikisito <46540697+Rikisito@users.noreply.github.com>
2019-02-04 19:10:14 +0100

    Add files via upload

---
+++ tablanotas.sql
@@ -0,0 +1,13 @@
+﻿create table notas_alumnos (Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+asignatura VARCHAR(45) NOT NULL ,
+nota DECIMAL(4, 2) NOT NULL ,
+curso CHAR(10) NOT NULL ,
+alumno VARCHAR(50) NOT NULL
+)DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+insert into notas_alumnos values(null,'Lengua', '4.00', 'ESO2', 'Luis Pérez Sanchez'),
+(null, 'Matemáticas', '4.5', 'ESO1', 'Ana López López'),
+(null, 'Matemáticas', '7.00', 'ESO1', 'Sandra Sol Ruiz'),
+(null, 'Matemáticas', '3.00', 'ESO2', 'Felipe Gómez López'),
+(null, 'Lengua', '8.00', 'ESO2', 'Juan Ruiz Ruiz'),
+(null, 'Lengua', '5.00', 'ESO1', 'María Ruiz Ruiz');
---
+++ tablausuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuarios (
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+nombre VARCHAR(50) NOT NULL ,
+PRIMARY KEY (id)
+);
+
+INSERT INTO usuarios ( nombre ) VALUES
+('Jose Perez Perez'),
+('Maria Ruiz Ruiz')
+;
